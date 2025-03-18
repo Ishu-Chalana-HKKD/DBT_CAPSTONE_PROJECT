a913e2ca615005cc361f62ef2d91dbc2b1ce3949
Khushi-Bhatia-HKKD <khushi_bhatia@hakkoda.io>
2025-03-18 13:55:14 +0530

    update till snapshots

---
+++ dbt/snapshots/boardgames_users.sql
@@ -0,0 +1,21 @@
+{% snapshot users %}
+
+{{
+    config(
+      target_schema='DBT_KBhatia',
+      unique_key='_line',
+      strategy='check',
+      check_cols = 'all'
+    )
+}}
+
+select 
+    _line,
+   "users",
+    "url",
+    "country",
+    "_fivetran_synced"
+ 
+from {{ source('boardgame', 'users') }}
+
+{% endsnapshot %}
